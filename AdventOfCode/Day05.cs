f7c2dd6644560202917ac3eb99fa5ce3b0d8a3d1
jlintvedt <jlintvedt@gmail.com>
2023-12-05 14:53:39 +0100

    Solve Day05 Puzzle1

--- AdventOfCode/Day05.cs
+++ AdventOfCode/Day05.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace AdventOfCode
 {
@@ -7,10 +8,100 @@ namespace AdventOfCode
     /// </summary>
     public class Day05
     {
+        public class SeedPlanting
+        {
+            public List<long> Seeds = new List<long>();
+            public List<Mapping> Maps = new List<Mapping>();
+
+            public SeedPlanting(string input)
+            {
+                var comp = input.Split($"{Environment.NewLine}{Environment.NewLine}");
+
+                foreach (var num in comp[0][7..].Split(' '))
+                    Seeds.Add(long.Parse(num));
+
+                for (int i = 1; i < comp.Length; i++)
+                    Maps.Add(new Mapping(comp[i]));
+            }
+
+            public long FindLowestLocationNumber()
+            {
+                var lowest = long.MaxValue;
+                foreach (var seed in Seeds)
+                {
+                    var dest = GetDestinationOfSeed(seed);
+                    lowest = lowest < dest ? lowest : dest;
+                }
+
+                return lowest;
+            }
+
+            private long GetDestinationOfSeed(long seed)
+            {
+                foreach (var map in Maps)
+                    seed = map.GetMapping(seed);
+
+                return seed;
+            }
+
+
+            public class Mapping
+            {
+                public string Type;
+                public List<Converter> Converters = new List<Converter>();
+
+                public Mapping(string input)
+                {
+                    var comp = input.Split(Environment.NewLine);
+                    Type = comp[0].Split(' ')[0];
+
+                    for (int i = 1; i < comp.Length; i++)
+                    {
+                        var raw = comp[i].Split(' ');
+                        Converters.Add(new Converter(long.Parse(raw[1]), long.Parse(raw[0]), long.Parse(raw[2])));
+                    }
+                }
+
+                public long GetMapping(long source)
+                {
+                    foreach (var conv in Converters)
+                        if (conv.TryConvert(source, out long newValue))
+                            return newValue;
+
+                    return source;
+                }
+            }
+
+            public class Converter
+            {
+                public long SourceStart, DestinationStart, Range;
+
+                public Converter(long sourceStart, long destinationStart, long range)
+                {
+                    SourceStart = sourceStart;
+                    DestinationStart = destinationStart;
+                    Range = range;
+                }
+
+                public bool TryConvert(long source, out long destination)
+                {
+                    if (source >= SourceStart && source < SourceStart+Range)
+                    {
+                        destination = DestinationStart + (source - SourceStart);
+                        return true;
+                    }
+
+                    destination = 0;
+                    return false;
+                }
+            }
+        }
+
         // == == == == == Puzzle 1 == == == == ==
         public static string Puzzle1(string input)
         {
-            return "Puzzle1";
+            var sp = new SeedPlanting(input);
+            return sp.FindLowestLocationNumber().ToString();
         }
 
         // == == == == == Puzzle 2 == == == == ==
